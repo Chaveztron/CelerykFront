26f99b39c62049ec5b17c757ef8cdf1b8cc1150e
Jesus Chavez <jesus.chavez.caro@uabc.edu.mx>
2021-12-31 00:25:15 -0800

    Acomodo de la parte pricipal del catalogo de componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import { Nav } from 'rsuite';
 import { HashRouter as Router, Switch, Route, Link, useLocation } from 'react-router-dom';
 
 import Sidenav from 'rsuite/Sidenav';
 import Dropdown from 'rsuite/Dropdown';
 
+import Navbar from 'rsuite/Navbar';
+
+import Sidebar from 'rsuite/Sidebar';
+import Content from 'rsuite/Content';
+import Container from 'rsuite/Container';
+import Header from 'rsuite/Header';
+
 // eslint-disable-next-line react/display-name
+
 const NavLink = React.forwardRef((props, ref) => {
   const location = useLocation();
   return (
@@ -16,35 +24,29 @@ const NavLink = React.forwardRef((props, ref) => {
 const styles = {
   width: 240,
   display: 'inline-table',
-  marginRight: 10
+  marginRight: 10,
+
 };
 
 const MySidenav = ({ appearance, ...props }) => {
   return (
     <div style={styles}>
-      <Sidenav appearance={appearance} defaultOpenKeys={['3', '4']}>
+      <Sidenav appearance={appearance} defaultOpenKeys={['3']}>
         <Sidenav.Body>
           <Nav {...props}>
-            <Nav.Item eventKey="1" active >
-              Dashboard
-            </Nav.Item>
-            <Nav.Item eventKey="2" >
-              User Group
-            </Nav.Item>
-            <Dropdown eventKey="3" title="Advanced" >
-              <Dropdown.Item eventKey="3-1">Geo</Dropdown.Item>
-              <Dropdown.Item eventKey="3-2">Devices</Dropdown.Item>
-              <Dropdown.Item eventKey="3-3">Loyalty</Dropdown.Item>
-              <Dropdown.Item eventKey="3-4">Visit Depth</Dropdown.Item>
+
+            <Dropdown eventKey="3" title="Formulario" >
+            <Dropdown.Item eventKey="3-1" href="#/form">Form</Dropdown.Item>
             </Dropdown>
-            <Dropdown eventKey="4" title="Settings">
-              <Dropdown.Item eventKey="4-1">Applications</Dropdown.Item>
-              <Dropdown.Item eventKey="4-2">Channels</Dropdown.Item>
-              <Dropdown.Item eventKey="4-3">Versions</Dropdown.Item>
-              <Dropdown.Menu eventKey="4-5" title="Custom Action">
-                <Dropdown.Item eventKey="4-5-1">Action Name</Dropdown.Item>
-                <Dropdown.Item eventKey="4-5-2">Action Params</Dropdown.Item>
-              </Dropdown.Menu>
+            <Dropdown eventKey="4" title="Componentes">
+              <Dropdown.Item eventKey="4-1" href="#/inputs">Inputs</Dropdown.Item>
+              <Dropdown.Item eventKey="4-2" href="#/inputs">Dropdown-list</Dropdown.Item>
+              <Dropdown.Item eventKey="4-3">Etiquetas</Dropdown.Item>
+              <Dropdown.Item eventKey="4-4">Calendario</Dropdown.Item>
+              <Dropdown.Item eventKey="4-5">Radio-Button</Dropdown.Item>
+              <Dropdown.Item eventKey="4-6">Checkbox</Dropdown.Item>
+              <Dropdown.Item eventKey="4-7">Botones</Dropdown.Item>
+
             </Dropdown>
           </Nav>
         </Sidenav.Body>
@@ -53,6 +55,28 @@ const MySidenav = ({ appearance, ...props }) => {
   );
 };
 
+function Home() {
+  return (
+    <Fragment>
+      <h2>Home</h2> 
+      <p>Elija unas de las opciones del sidebar</p>
+    </Fragment>
+  );
+}
+
+function Form() {
+  return <h2>Form</h2>;
+}
+
+function Inputs() {
+  return <h2>Inputs</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+
 export default function App() {
 
   //https://rsuitejs.com/components/sidenav/
@@ -60,46 +84,56 @@ export default function App() {
   const [openKeys, setOpenKeys] = React.useState(['3', '4']);
 
   return (
-    <Router>
-      <div>
-        <Nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <NavLink to="/users">Users</NavLink>
-        </Nav>
+<Router>
+  <Header>
+      <Navbar appearance="default">
+      <Navbar.Brand href="#">
+        CELERYK
+      </Navbar.Brand>
+          <Nav>
+            <Nav.Item ><NavLink to="/">Inicio</NavLink></Nav.Item>
+            <Nav.Item>Documentacion</Nav.Item>
+            <Nav.Item>Acerca de..</Nav.Item>
+            <Dropdown title="Mas">
+              <Dropdown.Item>Company</Dropdown.Item>
+              <Dropdown.Item>Team</Dropdown.Item>
+              <Dropdown.Item>Contact</Dropdown.Item>
+            </Dropdown>
+          </Nav>
+          <Nav pullRight>
+            <Nav.Item>Settings</Nav.Item>
+          </Nav>
+      </Navbar>
+  </Header>
 
-        <MySidenav
-        activeKey={activeKey}
-        openKeys={openKeys}
-        onSelect={setActiveKey}
-        onOpenChange={setOpenKeys}
-        appearance="inverse"
-      />
+      <Container>
+            <Sidebar>
+                <MySidenav
+                activeKey={activeKey}
+                openKeys={openKeys}
+                onSelect={setActiveKey}
+                onOpenChange={setOpenKeys}
+                appearance="inverse"
+              />
+            </Sidebar>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+              <Content>
+                  <Switch>
+                    <Route path="/form">
+                      <Form />
+                    </Route>
+                    <Route path="/inputs">
+                      <Inputs />
+                    </Route>
+                    <Route path="/users">
+                      <Users />
+                    </Route>
+                    <Route path="/">
+                      <Home />
+                    </Route>
+                  </Switch>
+              </Content>
+      </Container>
+  </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
